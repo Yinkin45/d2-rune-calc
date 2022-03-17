2f6ad7612c23809f3fec15eb7c4ac1b89b473873
Mike Rogers <mtr293@gmail.com>
2022-03-17 00:14:04 -0400

    added runeword list and css styling

---
+++ components/RunewordListItem.js
@@ -0,0 +1,11 @@
+import { Fragment } from 'react'
+import styles from './RunewordListItem.module.css'
+export default function RunewordListItem(props) {
+  return (
+    <Fragment>
+      <div className={styles.runeword}>
+        {props.runeword.name} - {props.runeword.type}
+      </div>
+    </Fragment>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import { Fragment } from 'react'
+import RunewordList from '../components/RunewordList'
+const RUNEWORDS = [
+  { name: 'Smite', type: 'shield' },
+  { name: 'Dodge', type: 'armor' },
+]
 
 export default function Home() {
   return (
     <Fragment>
-      <div>hello world</div>
+      <RunewordList runes={RUNEWORDS}></RunewordList>
     </Fragment>
   )
 }
