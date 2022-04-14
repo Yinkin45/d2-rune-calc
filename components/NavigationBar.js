1ee9350f6527b418d31c5f233ccb329711448012
Mike Rogers <mtr293@gmail.com>
2022-04-13 23:21:51 -0400

    added navbar and link CSS classes

--- components/NavigationBar.js
+++ components/NavigationBar.js
@@ -1,27 +1,28 @@
 import Link from 'next/link'
+import styles from './NavigationBar.module.css'
 
 export default function NavigationBar() {
   return (
-    <>
+    <div className={styles.navbar}>
       <Link href="/">
-        <a>Home</a>
+        <a className={styles.link}>Home</a>
       </Link>
 
       <Link href="/runewords">
-        <a>Runewords</a>
+        <a className={styles.link}>Runewords</a>
       </Link>
 
       <Link href="/calc">
-        <a>Calc</a>
+        <a className={styles.link}>Calc</a>
       </Link>
 
       <Link href="/profiles">
-        <a>Profiles</a>
+        <a className={styles.link}>Profiles</a>
       </Link>
 
       <Link href="/feedback">
-        <a>Feedback</a>
+        <a className={styles.link}>Feedback</a>
       </Link>
-    </>
+    </div>
   )
 }
