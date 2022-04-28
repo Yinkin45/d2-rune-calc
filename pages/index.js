e0b5b5d6dd0983e375e7ac97f0b0d1a5a883e2b6
Mike Rogers <mtr293@gmail.com>
2022-04-27 21:36:44 -0400

    Added content to the home page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,31 @@
+import Link from 'next/link'
+
 export default function Home() {
-  return <>Home Page</>
+  return (
+    <>
+      <h1>Introduction</h1>
+      <p>
+        Greetings Diablo 2 fans, I created this app to help players calculate
+        runes, help decide players which runewords are best for their
+        characters, and to provide information on runewords
+      </p>
+      <h1>What&#39;s New</h1>
+      <ul>
+        <li>New thing</li>
+        <li>New thing</li>
+        <li>new thing</li>
+      </ul>
+
+      <h1>What&#39;s Coming Up</h1>
+      <ul>
+        <li>New stuff coming up</li>
+        <li>New stuff coming up</li>
+        <li>New stuff coming up</li>
+      </ul>
+
+      <p>
+        Go to <Link href="/feedback">Feedback</Link>
+      </p>
+    </>
+  )
 }
